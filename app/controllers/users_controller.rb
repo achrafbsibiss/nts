c90d2d24c8b7ad1b95bd95b5f5a041da0e54a511
Achraf <achraf@grh.com>
2023-09-22 13:06:36 +0100

    Working in the users show all the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: %i[show edit update destroy]
   before_action :check_admin_privileges, only: %i[new create]
+
+  def index
+    @users = User.all
+  end
   
   def show
   end 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:site) { create(:site) }
+
+  let(:validat_attributes)do
+    {
+      first_name: "John",
+      last_name: "Doe",
+      email: "me@test.com",
+      phone: "12345678",
+      birthdate: "02-03-1998",
+      gender: "male",
+      site_id: site.id,
+      cin: "ZWE452GH",
+      job_title: "Acheteur"
+    }
+  end
+
+  let(:invalid_attributes) do
+    {
+      first_name: "",
+      last_name: "",
+      phone: "",
+      birthdate: ""
+    }
+  end
+  before do
+    Role.create!(user_id: user.id, name: :admin)
+  end
+  
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
