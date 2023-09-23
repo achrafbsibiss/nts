842ae1b8b26beb8f145c32d872cf726c2ace4aae
Achraf <achraf@grh.com>
2023-09-23 14:11:43 +0100

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(
+  email: 'netis_group@gmail.com',
+  password: 'Netis_12345',
+  last_name: 'Nizar',
+  first_name: 'belmahfoud',
+  phone: '065367489',
+  nationality: "Morocco",
+  cin: "j2902",
+  gender: "male",
+  birthdate: "1992-02-02"
+)
+
+
+Role.create(
+  user_id: 1,
+  name: 1
+)
